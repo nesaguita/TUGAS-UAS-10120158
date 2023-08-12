163076044a6ca0138a574edb7211e024cd75f8db
nesaguita <nesaguita.com>
2023-08-12 23:47:56 +0700

    first commit

---
+++ app/src/main/java/com/example/notes/Note.java
@@ -0,0 +1,38 @@
+package com.example.notes;
+
+
+import com.google.firebase.Timestamp;
+
+public class Note {
+    public int content;
+    String title;
+    String Content;
+    Timestamp timestamp;
+
+    public Note() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return Content;
+    }
+
+    public void setContent(String content) {
+        Content = content;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+}
